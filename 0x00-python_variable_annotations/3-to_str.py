4618ee847744e055804774596ceb09457257c67b
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-05 15:17:15 +0100

    3-to_str.py done

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module that takes a float n as argument and returns the string"""
 
+
 def to_str(n: float) -> str:
     """Returns the string representation of the float"""
     return str(n)
