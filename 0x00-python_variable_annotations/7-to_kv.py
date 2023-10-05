34e4aa8fbdf46ddfab06a11572f5fb6caa132c41
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-05 15:41:10 +0100

    7-to_kv.py done

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Module that takes a string k and an int OR
+   float v as arguments and returns a tuple'''
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Returns a tuple'''
+    return (k, v * v)
