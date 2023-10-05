9208a7ba26d0c50ac8211ae66fe21bd960868688
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-05 15:00:42 +0100

    2-floor.py done

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 '''Basic annotations - floor'''
+import math
 
 def floor(n: float) -> int:
     '''Returns the floor of the float'''
-    return n // 1
+    return int(n // 1)
+ans = floor(3.14)
+
+print(ans == math.floor(3.14))
+print(floor.__annotations__)
+print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
