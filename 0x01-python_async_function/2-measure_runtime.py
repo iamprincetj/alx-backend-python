2605cb7bb210dd542881ea654d8e341e98be4197
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-09 14:46:59 +0100

    2-measure_runtime.py updated

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def measure_time(n: int, max_delay: int) -> float:
     '''Asynchronous coroutine that measures the total execution time
         for wait_n(n, max_delay), and returns total_time / n'''
-    start = time.perf_counter()
+    start = time.time()
     await wait_n(n, max_delay)
-    end = time.perf_counter()
+    end = time.time()
     return (end - start) / n
